ac1c1e5f2922e3ba55c05d5f6e1ddbdbb86d72bb
alexchegash <aliskander84@gmail.com>
2020-06-08 18:29:59 +0300

    added raw user cards with hover action

---
+++ src/components/UserCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-hover>
+    <template v-slot="{hover}">
+      <v-card
+          class="user-card"
+          :elevation="hover ? 4 : 1"
+      >
+        <v-card-title>
+          {{user.first_name}} {{user.last_name}}
+        </v-card-title>
+      </v-card>
+    </template>
+  </v-hover>
+</template>
+
+<script>
+  export default {
+    name: 'UserCard',
+    props: {
+      user: Object
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .user-card {
+
+  }
+</style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,14 +1,24 @@
 <template>
-  <div class="users">
-    {{users}}
+  <div class="users pa-4">
+    <UserCard
+      v-for="user of users"
+      :key="user.id"
+      :user="user"
+      :v-if="users.length !== 0"
+    />
+    <div class="loader" v-if="users.length === 0">
+      Empty
+    </div>
   </div>
 </template>
 
 <script>
   import {mapActions, mapGetters} from 'vuex'
+  import UserCard from '../components/UserCard'
 
   export default {
     name: 'Users',
+    components: {UserCard},
     data() {
       return {
         pageNumber: 1
